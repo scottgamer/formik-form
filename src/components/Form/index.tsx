49388d89b2a9a84436962a12507b3d817db6dbba
rmunoz <richosojason@msn.com>
2021-03-15 22:25:32 -0500

    added form validations

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import validationSchema from "./validationSchema";
 
 import "./styles.css";
 
@@ -25,24 +26,56 @@ const index = (): JSX.Element => {
   return (
     <>
       <h2>My first Formik form</h2>
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
         <Form className="form">
           <label htmlFor="firstName" className="form__label">
             First Name
           </label>
-          <Field name="firstName" type="text" className="form__input" placeholder="First Name"/>
+          <Field
+            name="firstName"
+            type="text"
+            className="form__input"
+            placeholder="First Name"
+          />
+          <ErrorMessage
+            name="firstName"
+            component="span"
+            className="form__error"
+          />
 
           <label htmlFor="lastName" className="form__label">
             Last Name
           </label>
-          <Field name="lastName" type="text" className="form__input" placeholder="Last Name"/>
+          <Field
+            name="lastName"
+            type="text"
+            className="form__input"
+            placeholder="Last Name"
+          />
+          <ErrorMessage
+            name="lastName"
+            component="span"
+            className="form__error"
+          />
 
           <label htmlFor="email" className="form__label">
             Email
           </label>
-          <Field name="email" type="text" className="form__input" placeholder="Email"/>
+          <Field
+            name="email"
+            type="text"
+            className="form__input"
+            placeholder="Email"
+          />
+          <ErrorMessage name="email" component="span" className="form__error" />
 
-          <button type="submit" className="btn">Submit!</button>
+          <button type="submit" className="btn">
+            Submit!
+          </button>
         </Form>
       </Formik>
     </>
