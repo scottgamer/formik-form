3c69b26953d158e1782afa942fc594138a3e4646
rmunoz <richosojason@msn.com>
2021-03-15 19:51:44 -0500

    added styles and placeholders to form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Formik, Form, Field } from "formik";
+
+import "./styles.css";
 
 interface FormValues {
   firstName: string;
@@ -23,17 +26,23 @@ const index = (): JSX.Element => {
     <>
       <h2>My first Formik form</h2>
       <Formik initialValues={initialValues} onSubmit={onSubmit}>
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field name="firstName" type="text" />
+        <Form className="form">
+          <label htmlFor="firstName" className="form__label">
+            First Name
+          </label>
+          <Field name="firstName" type="text" className="form__input" placeholder="First Name"/>
 
-          <label htmlFor="lastName">Last Name</label>
-          <Field name="lastName" type="text" />
+          <label htmlFor="lastName" className="form__label">
+            Last Name
+          </label>
+          <Field name="lastName" type="text" className="form__input" placeholder="Last Name"/>
 
-          <label htmlFor="email">Email</label>
-          <Field name="email" type="text" />
+          <label htmlFor="email" className="form__label">
+            Email
+          </label>
+          <Field name="email" type="text" className="form__input" placeholder="Email"/>
 
-          <button type="submit">Submit!</button>
+          <button type="submit" className="btn">Submit!</button>
         </Form>
       </Formik>
     </>
