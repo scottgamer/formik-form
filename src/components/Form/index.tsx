2d042b1a0c8e1ccb2423e48f132c4a132e3a85a1
rmunoz <richosojason@msn.com>
2021-03-15 19:32:13 -0500

    added initial values and form fields

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 
+interface FormValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
 const index = (): JSX.Element => {
-  return <div>Le form</div>;
+  const initialValues: FormValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+  };
+
+  const onSubmit = (values: FormValues) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+    }, 500);
+  };
+
+  return (
+    <>
+      <h2>My first Formik form</h2>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" type="text" />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" type="text" />
+
+          <label htmlFor="email">Email</label>
+          <Field name="email" type="text" />
+
+          <button type="submit">Submit!</button>
+        </Form>
+      </Formik>
+    </>
+  );
 };
 
 export default index;
