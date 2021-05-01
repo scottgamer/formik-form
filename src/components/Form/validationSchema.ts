335277764288ca72d574f6aef1718c5a1b5725b9
rmunoz <richosojason@msn.com>
2021-04-30 19:23:07 -0500

    added bio validation

--- src/components/Form/validationSchema.ts
+++ src/components/Form/validationSchema.ts
@@ -1,9 +1,11 @@
 import * as yup from "yup";
 
 const isRequiredMessage = "This field is required";
+const maxCharMessage = "Must be less than 255 characters";
 
 export default yup.object().shape({
   firstName: yup.string().required(isRequiredMessage).max(10),
   lastName: yup.string().optional().max(10),
   email: yup.string().required(isRequiredMessage).email(),
+  bio: yup.string().optional().max(255, maxCharMessage),
 });
