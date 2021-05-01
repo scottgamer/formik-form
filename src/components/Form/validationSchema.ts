07d65acf97acf5ed138031fc310d5e4be1ec74ba
rmunoz <richosojason@msn.com>
2021-04-30 23:56:15 -0500

    added validation values

--- src/components/Form/validationSchema.ts
+++ src/components/Form/validationSchema.ts
@@ -2,10 +2,17 @@ import * as yup from "yup";
 
 const isRequiredMessage = "This field is required";
 const maxCharMessage = "Must be less than 255 characters";
+const minTechnologyMessage = "Select at least one";
 
 export default yup.object().shape({
   firstName: yup.string().required(isRequiredMessage).max(10),
   lastName: yup.string().optional().max(10),
   email: yup.string().required(isRequiredMessage).email(),
   bio: yup.string().optional().max(255, maxCharMessage),
+  technologies: yup
+    .array()
+    .required(isRequiredMessage)
+    .min(1, minTechnologyMessage),
+  coding_frequency: yup.string().required(isRequiredMessage),
+  receive_notifications: yup.string().required(isRequiredMessage),
 });
